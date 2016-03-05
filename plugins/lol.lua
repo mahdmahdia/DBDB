ef4946297c346ba636555cb0c6c7b9b4f18271cc
mahdmahdia <mahdi028c@gmail.com>
2016-03-05 13:59:34 +0330

    Create lol.lua

---
+++ plugins/lol.lua
@@ -0,0 +1,22 @@
+do
+
+function run(msg, matches)
+local reply_id = msg['id']
+
+local text = ':|'
+
+reply_msg(reply_id, text, ok_cb, false)
+end
+
+return {
+patterns = {
+"^😂",
+    "^😂😂",
+    "^😂😂😂",
+   "^😂😂😂",
+    "^>😂😂😐",
+},
+run = run
+}
+
+end
